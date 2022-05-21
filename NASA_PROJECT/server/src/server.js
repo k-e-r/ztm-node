c0a2c58674f6a7f29336e426ea69aaf06c075806
k-e-r <damajonboua@gmail.com>
2022-05-21 13:45:24 -0700

    add api server setup

---
+++ NASA_PROJECT/server/src/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+// app.listen();
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
