363a246e1472520586c0ac60326caf85288ee7fc
k-e-r <damajonboua@gmail.com>
2022-05-23 12:07:31 -0700

    add serving application with client side routing

--- NASA_PROJECT/server/src/app.js
+++ NASA_PROJECT/server/src/app.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -13,9 +15,11 @@ app.use(
 );
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(morgan('combined'));
 
 app.use(planetsRouter);
-app.get('/', (req, res) => {
+app.use(launchesRouter);
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
